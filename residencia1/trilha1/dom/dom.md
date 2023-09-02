6e4b900aab0e5c5b1f1369d813fddb42d9371af8
William Santana <williamandrade1058@gmail.com>
2023-09-01 21:51:53 -0300

    update trilha1

---
+++ residencia1/trilha1/dom/dom.md
@@ -0,0 +1,10 @@
+# DOM
+## Document Object Model
+
+---
+É uma interface que o html, css, js possam manipular para construir páginas e conteúdos através dos navegadores
+
+---
+Quem permite o uso é a W3C(Word Wide Web Consortium)
+
+---
---
+++ residencia1/trilha1/js/script.js
@@ -0,0 +1,28 @@
+const input_element = document.querySelector('input');
+const button_element = document.querySelector('form button');
+const ul_element = document.querySelector('ul');
+
+
+button_element.onclick = ev => {
+    ev.preventDefault();
+
+    if (input_element.value) {
+        const text_element = document.createElement('span');
+        text_element.innerHTML = input_element.value;
+
+        const btn_element = document.createElement('button');
+        btn_element.innerHTML = 'Remover';
+
+        const li_element = document.create_element('li');
+        li_element.appendChild(text_element);
+        li_element.appendChild(btn_element);
+
+        btn_element.onclick = () => {
+            ul_element.removeChild(li_element)
+        }
+
+        ul_element.appendChild(li_element);
+        input_element.value = '';
+    }
+
+};
