3b3e390dd32b7fbd233965bd3930001c80f24c54
William Santana <williamandrade1058@gmail.com>
2023-09-25 11:38:33 -0300

    adicionando javascript

---
+++ javascript/add_carrinho.js
@@ -0,0 +1,9 @@
+const add_carrinho = () => {
+
+    let result_pizza_calabresa = document.getElementById("result_pizza_calabresa")
+    let result_pizza_portuguesa = document.getElementById("result_pizza_portuguesa");
+
+    result_pizza_calabresa.textContent = 'Item adicionado'
+    result_pizza_portuguesa.textContent = 'Item adicionado'
+
+};
