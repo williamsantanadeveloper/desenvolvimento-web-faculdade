c16bcb6353693447f33133a44ff122e826acc196
William Santana <williamandrade1058@gmail.com>
2023-10-17 13:20:44 -0300

    add jogo_adivinhação parte 1

---
+++ prog_web_frontend/jogo_adivinhacao/parte1/js/functions.js
@@ -0,0 +1,34 @@
+const adivinhar_numero = () => {
+    let number = document.getElementById("number").value;
+    let number_predefinido = 25;
+    let result = document.getElementById("result");
+
+    if (number > 100 || number < 1) {
+        result.textContent = 'Informe um número entre 1 e 100'
+    }
+
+    if (number == number_predefinido) {
+        let contador = 0;
+        while (number != number_predefinido) {
+            contador++
+
+        }
+        result.textContent = 'Você acertou! Parabéns!'.concat('Você acertou em ', contador += 1, ' tentativas')
+    }
+
+    else if (number < number_predefinido) {
+        result.textContent = 'O número é maior'
+    }
+
+    else {
+        result.textContent = 'O número é menor'
+    }
+
+    // if (number == number_predefinido) {
+    //     result.textContent = 'Parabéns! Você adivinhou o número é '.concat(number_predefinido).concat('Você acertou em ').concat(contador).concat('tentativas')
+    // }
+
+    // else {
+    //     result.textContent = 'Tente novamente!'
+    // }
+};
