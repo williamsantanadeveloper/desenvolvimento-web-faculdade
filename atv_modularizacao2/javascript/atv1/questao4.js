6474b550088a8ba73424660e9a5f08cbf5063b36
William Santana <71521910+williamsantanadeveloper@users.noreply.github.com>
2023-11-06 20:52:42 -0300

    Add files via upload

---
+++ atv_modularizacao2/javascript/atv1/questao4.js
@@ -0,0 +1,5 @@
+const division = (x,y) => {
+    return x / y;
+};
+
+console.log(division(3,2));
---
+++ atv_modularizacao2/javascript/atv1/questao7.js
@@ -0,0 +1,10 @@
+const menorNumero = (numero1, numero2) => {
+    if (numero1 < numero2) {
+        return 'O primero número é menor'
+    }
+    else {
+        return 'O segundo número é menor'
+    }
+};
+
+console.log(maiorNumero(4,5));
